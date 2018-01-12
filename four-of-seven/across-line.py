c1fa1ba0681a27180208219db6df9711b7c30685
unknown <cium5570@WFXMZBD2.internal.usyd.edu.au>
2018-01-12 12:45:32 +1100

    Added initial line crossing detectiong file.

---
+++ four-of-seven/across-line.py
@@ -0,0 +1,20 @@
+from microbit import *
+
+def across_line():
+    return False
+    
+while True:
+    if across_line():
+        break
+        
+    pin0.write_digital(1)
+    pin16.write_digital(0)
+    pin12.write_digital(0)
+    pin8.write_digital(1)
+    sleep(5)
+    pin0.write_digital(0)
+    pin16.write_digital(0)
+    pin12.write_digital(0)
+    pin8.write_digital(0)    
+    sleep(15)
+    
