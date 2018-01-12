2af5a018e505743b25e7af38b5b1d19c24b9e595
James Wright <jamesthefrequentlynotwright@gmail.com>
2018-01-12 17:42:43 +1100

    Added individual motor control, and pushup threshold.

--- four-of-seven/push_up_control.py
+++ four-of-seven/push_up_control.py
@@ -1,20 +1,45 @@
-'''
+"""
 This program is made for the pushuppers controlling the robot
 It transmits accelerometer data on channel 34
-'''
+"""
+
 
 from microbit import *
 import radio
 
+
+CHANNEL = 34
+PUSH_UP_ACCELERATION_THRESHOLD = 1200
+MOTORS = ['L', 'R']
+ROBOTS = ['A']
+
+
 radio.on()
-radio.config(channel=34) # sets channel to 34
+radio.config(channel=CHANNEL) # sets channel to 34
+
+
+was_over_threshold = abs(accelerometer.get_z()) >= \
+    PUSH_UP_ACCELERATION_THRESHOLD
+motor_index = 0
+robot_index = 0
+get_robot_id = lambda: ROBOTS[robot_index % len(ROBOTS)]
+get_motor_id = lambda: MOTORS[motor_index % len(MOTORS)]
+
 
 while True:
-    x = accelerometer.get_x()
-    y = accelerometer.get_y()
-    z = accelerometer.get_z()
-    print("x, y, z:", x, y, z)
-    sleep(50)
-    radio.send(str((x, y, z)))
-    if z > 2048:
-        radio.send(True)
+    if button_a.was_pressed():
+        robot_index += 1
+    if button_b.was_pressed():
+        motor_index += 1
+    
+    display.show([get_robot_id(), get_motor_id()])
+
+    z_axis_acceleration = accelerometer.get_z()
+    print(z_axis_acceleration)
+    if abs(z_axis_acceleration) >= PUSH_UP_ACCELERATION_THRESHOLD and not \
+            was_over_threshold:
+        was_over_threshold = True
+        radio.send(get_motor_id())
+        print('Transmitted {}.'.format(get_motor_id()))
+    elif abs(z_axis_acceleration) < PUSH_UP_ACCELERATION_THRESHOLD:
+        was_over_threshold = False
