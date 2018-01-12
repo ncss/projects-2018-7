fcc82b9a3359bb207d225e47a1510d79889b44c5
davidchung <davidchung89@gmail.com>
2018-01-12 15:22:32 +1100

    Serial send sample - press a, b or both
    Serial send sample - press a, b or both

--- SerialRead_PC/serialRead.py
+++ SerialRead_PC/serialRead.py
@@ -8,16 +8,21 @@ from time import sleep
 ser = serial.Serial('COM5', 115200, timeout=0)
 #ser.open()
 while True:
-    data = ser.readline()
-    if len(data) > 0:
+	data = ser.readline().strip()
+	if len(data) > 0:
+		print (data)
 		#when data "a" stays in received data open webbrowser with the given link 
-        if("a" in str(data)):
-            webbrowser.open("https://youtu.be/WApIDsZaNfI")
+		if("'a'" in str(data)):
+			webbrowser.open("https://youtu.be/WApIDsZaNfI")
 		#when data "b" stays in received data kill chrome.exe (This will kill all windows and tabs)
-        elif("b" in str(data)):            
-            browserExe = "chrome.exe"    
-            os.system("taskkill /f /im "+browserExe)
-    sleep(0.5)
+		elif("'b'" in str(data)):
+			browserExe = "chrome.exe"    
+			os.system("taskkill /f /im "+browserExe)
+		#when data "c" stays in received data open webbrowser with the given link  (sample for TTS)
+		if("'c'" in str(data)):
+			msg = "Hello there, this is testing."
+			webbrowser.open("http://ncss.davidchungproject.com/?msg=" + msg)
+	sleep(0.5)
 			
 ser.close()
 
