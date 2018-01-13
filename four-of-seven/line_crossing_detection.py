fbdcdbe982f1b353c56d0e5777b909093443a804
CindyUm <cindy.um73@gmail.com>
2018-01-13 14:34:28 +1100

    updated_line_crossing_detection

--- four-of-seven/line_crossing_detection.py
+++ four-of-seven/line_crossing_detection.py
@@ -1,11 +1,22 @@
 from microbit import *
 
 def across_line():
+    
+    light_1 = pin1.read_analog()
+    light_2 = pin2.read_analog()
+    
+    print ("light_1:" + str(light_1))
+    print ("light_2:" + str(light_2))
+    
+    if light_1 < 10 and light_2 <10:
+        return True
+    
     return False
     
 while True:
     if across_line():
-        break
+        print("STOP")
+        sleep(2000)
         
     pin0.write_digital(1)
     pin16.write_digital(0)
