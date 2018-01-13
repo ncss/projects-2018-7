5dbce1b4c20ce5e4ab88df39e38adbb708427da3
wlin9118 <michiamowendy@hotmail.com>
2018-01-14 01:27:36 +1100

    final

--- Uruguay Potato Vine/Streetfighter, receiving and display.py
+++ Uruguay Potato Vine/Streetfighter, receiving and display.py
@@ -11,6 +11,8 @@ punch = -20
 sideways_jab = -30
 fireball = -40
 
+display.show(Image.HAPPY)
+
 
 insults= ['Your mother was a hampster and your father smelled of elderberries.', 
 'His wit\'s as thick as a Tewkesbury mustard.', 'I am sick when I do look on thee.', 
@@ -20,8 +22,8 @@ insults= ['Your mother was a hampster and your father smelled of elderberries.',
 'You are as a candle, the better burnt out.', 
 'Thou appeareth nothing to me but a foul and pestilent congregation of vapours.', 
 ]
-
-while True:
+gameon = True 
+while gameon == True:
     '''if button_a.was_pressed():
         music.play(music.ENTERTAINER)
         radio.on()
@@ -41,16 +43,23 @@ while True:
             print(answer)
             health_2 += punch
             print('Player Two Health: ' + str(health_2)+ '\n')
+            radio.send('player2_health:{}'.format(health_2))
         if answer == "Sideways jab from player 1":
             print(answer)
             health_2 += sideways_jab
-            print('Player Two Health: ' + str(health_2)+ '\n')            
+            print('Player Two Health: ' + str(health_2)+ '\n')
+            radio.send('player2_health:{}'.format(health_2))
         if answer == "Fireball from player 1":
             print(answer)
             health_2 += fireball
             print('Player Two Health: ' + str(health_2) + '\n')
+            radio.send('player2_health:{}'.format(health_2))
         if answer == "Insult from player 1":
-            choice = random.choice(insults)
+            choice = random.choice(insults) 
+            radio.send("Player 1 says: " + choice) 
+            ttsChoice = random.randrange(0,7)
+            radio.send("TTS:" + str(ttsChoice)) 
+            
             print("Player 1 says: " + choice + '\n')
             if choice == insults[0]:
                 music.play(music.PYTHON, wait=False)
@@ -60,33 +69,39 @@ while True:
             print(answer)
             health_1 += punch
             print('Player One Health: ' + str(health_1)+ '\n')
+            radio.send('player1_health:{}'.format(health_1))
         if answer == "Sideways jab from player 2":
             print(answer)
             health_1 += sideways_jab
-            print('Player One Health: ' + str(health_1)+ '\n')            
+            print('Player One Health: ' + str(health_1)+ '\n')  
+            radio.send('player1_health:{}'.format(health_1))
         if answer == "Fireball from player 2":
             print(answer)
             health_1 += fireball
-            print('Player One Health: ' + str(health_1)+ '\n')            
+            print('Player One Health: ' + str(health_1)+ '\n')
+            radio.send('player1_health:{}'.format(health_1))
         if answer == "Insult from player 2":
             choice = random.choice(insults)
+            radio.send("Player 2 says: " + choice) 
+            ttsChoice = random.randrange(0,7)
+            radio.send("TTS:" + str(ttsChoice))
             print("Player 2 says: " + choice + '\n')
             if choice == insults[0]:
                 music.play(music.PYTHON, wait=False)
         
-            
+        loop = 0    
         #player 1
         if health_2 <= 0:
-            display.show(Image.HAPPY)
-            music.play(music.POWER_UP)
+            while loop < 10:  
+                radio.send("player1_win")
+                loop+=1
+                sleep(100)
+            gameon = False 
         if health_1 <= 0:
-            display.show(Image.SAD)
-            music.play(music.WAWAWAWAA)
-
-'''#player 2
-if health_1 <= 0:
-    display.show(Image.HAPPY)
-    music.play(music.POWER_UP)
-if helath_2 <=0:
-    display.show(Image.SAD)
-    music.play(music.WAWAWAWAA)'''
+            while loop < 10:
+                radio.send("player2_win")
+                loop+=1
+                sleep(100)
+            gameon = False 
+            
+display.show(Image.SAD)
