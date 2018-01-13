e70f462fd741fecca83cd9e1d77a006e6812901e
James Wright <jamesthefrequentlynotwright@gmail.com>
2018-01-13 20:27:25 +1100

    Finished colour light sensing.

--- four-of-seven/color_light_sensors.py
+++ four-of-seven/color_light_sensors.py
@@ -1,46 +1,28 @@
 from microbit import *
 
-order = ['Red', 'White', 'Green']
-status = {'Red': False,
-        'White': False,
-        'Green': False}
 
-Range = {'Red':[600, 630], 
-        'White': [500, 530],
-        'Green': [400, 430]}
-    
+# If any sensor is seeing brighted (lower) than this, it can be considered on.
+THRESHOLD = 590
+# If red and green are on, that is white, if they are both off, that is red.
+RED_PIN = pin1
+GREEN_PIN = pin2
+
+
+detected_colour = lambda pin: pin.read_analog() < THRESHOLD
+on_red = lambda: detected_colour(RED_PIN) and not detected_colour(GREEN_PIN)
+on_green = lambda: detected_colour(GREEN_PIN) and not detected_colour(RED_PIN)
+on_white = lambda: detected_colour(GREEN_PIN) and detected_colour(RED_PIN)
+
+
+order = [on_red, on_green, on_white]
+position_in_order = 0
+
 while True:
-    light_1 = pin1.read_analog()
-    light_2 = pin2.read_analog()
-    print('red:' + str(light_1))
-    print('green:' + str(light_2)) 
-    sleep(500)
-    
-    for color, colRange in Range.items(): 
-        if light_1 > colRange[0] and light_1 < colRange[1] and light_2 > colRange[0] and light_2 < colRange[1]: 
-            
-            status [color] = True 
-            
+    print(position_in_order)
+    if position_in_order < len(order) and order[position_in_order]():
+        position_in_order += 1
+    if position_in_order >= len(order):
+        print("Finished")
 
-def is_passed(currentColor):
-    currentInd = currentcolor
-    
-    '''
-    if light_1 > Red[0] and light_1 < Red[1] and light_2 > Red[0] and light_2 < Red[1]: 
-        status ['Red'] = True 
-        
-    if light_1 > White[0] and light_1 < White[1] and light_2 > White[0] and light_2 < White[1]: 
-        status ['White'] = True 
-    
-    if light_1 > Green[0] and light_1 < Green[1] and light_2 > Green[0] and light_2 < Green[1]: 
-        status ['Green'] = True 
-        
-    
-    #if light > 500
-        counter =+ 1 
-    #else 
-        display.show(Image.SAD)
-    
-    #if light < XXX
-    '''
-    
+    if button_a.was_pressed():
+        position_in_order = 0
